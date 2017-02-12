704dc63be15618a8b03e044635b1dd1eef3f9e6f
Oleksii Holyk <oleksii.holyk@outlook.com>
2017-02-12 22:16:43 +0200

    now we use another calendar type. admin pas/login save in code (test period)

--- src/main/java/ua/com/MvcConfig.java
+++ src/main/java/ua/com/MvcConfig.java
@@ -1,19 +1,15 @@
-package ua.com.springConfig;
+package ua.com;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-/**
- * Created by Oleksii on 28.01.2017.
- * oleksii.holyk@outlook.com
- */
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
+    
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
-        registry.addViewController("/hello").setViewName("hello");
-        registry.addViewController("/addEvent").setViewName("addEvent");
     }
+
 }

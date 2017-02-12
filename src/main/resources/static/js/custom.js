8424a218a35ad9c142d31d086c9b70dbb6b16a38
Oleksii Holyk <oleksii.holyk@outlook.com>
2017-02-12 23:59:46 +0200

    small refactor

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -4,12 +4,9 @@
 
 
 $(document).ready(function () {
-   
-
     $('.fc-row').each(function () {
         $(this).hide();
     });
-
     $('.fc-today').parents('.fc-row').show().css({"height": "50%"});
     $('.fc-today').parents('.fc-row').prev().show().css({"height": "50%"});
 
